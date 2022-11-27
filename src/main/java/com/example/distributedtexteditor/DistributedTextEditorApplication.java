1b2e628b4aa6d612d3608a485c920ddb40d8647c
kezumah <kennedyezumah@users-mbp.attlocal.net>
2022-11-26 22:34:50 -0800

    setup for web server

---
+++ src/main/java/com/example/distributedtexteditor/DistributedTextEditorApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributedtexteditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTextEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTextEditorApplication.class, args);
+	}
+
+}
