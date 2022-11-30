9279faddae042dc25eb3279df7b6217f16fd1b1c
kezumah <kennedyezumah@users-MacBook-Pro.local>
2022-11-29 23:05:56 -0800

    adding database controller

--- src/main/java/com/example/distributedtexteditor/controller/DatabaseController.java
+++ src/main/java/com/example/distributedtexteditor/controller/DatabaseController.java
@@ -1,2 +1,20 @@
-package com.example.distributedtexteditor.controller;public class DatabaseController {
+package com.example.distributedtexteditor.controller;
+
+import java.util.ArrayList;
+
+public class DatabaseController {
+
+    public static void createHashInDatabase(String hash){
+        // needs to be implemented
+    }
+
+    public static ArrayList<String> getDocFromDatabase(String hash) {
+        ArrayList<String> doc = new ArrayList<String>();
+        // needs to be implemented
+        return doc;
+    }
+
+    public static void saveDoc (String hash){
+        // needs to be implemented
+    }
 }
