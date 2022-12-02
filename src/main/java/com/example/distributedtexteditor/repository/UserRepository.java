db5dc6ad7b6a12c39c9bf19178fa0cae0282c35d
aditya yadav <yadav.ad@northeastern.edu>
2022-12-02 18:04:45 -0500

    swagger and h2 added

---
+++ src/main/java/com/example/distributedtexteditor/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.distributedtexteditor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.distributedtexteditor.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
