d26be384b776626f2eeca5261610d317e1f1069b
Di Zhang <88256896+diz28@users.noreply.github.com>
2022-12-05 17:36:37 -0800

    frontend is connected to server and can update and get data from and to database

--- src/main/java/com/example/distributedtexteditor/controller/AppServerController.java
+++ src/main/java/com/example/distributedtexteditor/controller/AppServerController.java
@@ -4,15 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.distributedtexteditor.entity.User;
 import com.example.distributedtexteditor.service.DatabaseController;
@@ -20,6 +12,7 @@ import com.example.distributedtexteditor.service.UserService;
 import com.example.distributedtexteditor.service.UtilityMethods;
 
 @RestController
+@CrossOrigin
 public class AppServerController {
 	@Autowired
 	private UserService userService;
--- src/main/text-editor-frontend/src/pages/ExistNote.js
+++ src/main/text-editor-frontend/src/pages/ExistNote.js
@@ -1,30 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function ExistNote() {
   const [code, setCode] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [infos, setInfos] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const noteCode = { code };
-    console.log(noteCode);
 
-    fetch("http://localhost:8080/doc", {
+    const info = { name, address };
+
+    fetch("http://localhost:9090/addUser", {
       method: "POST",
-      headers,
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(info),
+    }).then(() => {
+      console.log("new info added");
     });
   };
+
+  useEffect(() => {
+    fetch("http://localhost:9090/users")
+      .then((res) => res.json())
+      .then((result) => {
+        setInfos(result);
+      });
+  }, []);
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <lable>Note Code:</lable>
+        <lable>Name:</lable>
         <input
           type="text"
           required
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <lable> Address:</lable>
+        <input
+          type="text"
+          required
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
         />
         <button>Enter</button>
       </form>
+
+      {console.log(infos)}
+      {name}
+      {address}
+      <h2>INFO</h2>
+      {infos.map((information) => (
+        <div>
+          Address: {information.address}
+          ID: {information.id}
+          Name: {information.name}
+        </div>
+      ))}
     </div>
   );
 }
