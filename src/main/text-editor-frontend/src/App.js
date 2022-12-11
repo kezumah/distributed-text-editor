c6c1dc87102b339a4e2c93a259ce4dd17fd3bace
Di Zhang <88256896+diz28@users.noreply.github.com>
2022-12-11 00:11:49 -0800

    multiple local browsers can be synched

--- src/main/text-editor-frontend/src/App.js
+++ src/main/text-editor-frontend/src/App.js
@@ -6,24 +6,37 @@ import NewNote from "./pages/NewNote";
 import ExistNote from "./pages/ExistNote";
 import NotFound from "./pages/NotFound";
 import Post from "./pages/Post";
+import SynchingEditor from "./SynchingEditor";
+
 function App() {
   return (
-    <BrowserRouter basename="notepad">
-      <div className="App">
-        <h1>Note Pad</h1>
-        <Header />
-      </div>
-      <div className="App">
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/newnote" component={NewNote} exact />
-          <Route path="/existnote" component={ExistNote} exact />
-          <Route path="/existnote/post/:id" component={Post} exact />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div>
+      <SynchingEditor />
+      <br />
+
+      <SynchingEditor />
+      <br />
+
+      <SynchingEditor />
+    </div>
   );
+  // return (
+  //   <BrowserRouter basename="notepad">
+  //     <div className="App">
+  //       <h1>Note Pad</h1>
+  //       <Header />
+  //     </div>
+  //     <div className="App">
+  //       <Switch>
+  //         <Route path="/" component={Home} exact />
+  //         <Route path="/newnote" component={NewNote} exact />
+  //         <Route path="/existnote" component={ExistNote} exact />
+  //         <Route path="/existnote/post/:id" component={Post} exact />
+  //         <Route component={NotFound} />
+  //       </Switch>
+  //     </div>
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;
