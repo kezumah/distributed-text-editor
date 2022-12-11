dcf6a0e057dcb3e0233db3102e68b1b14031a941
aditya yadav <yadav.ad@northeastern.edu>
2022-12-10 19:31:07 -0500

    atlas mongo added

--- src/main/java/com/example/distributedtexteditor/controller/AppServerController.java
+++ src/main/java/com/example/distributedtexteditor/controller/AppServerController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.distributedtexteditor.entity.Doc;
 import com.example.distributedtexteditor.entity.Documents;
 import com.example.distributedtexteditor.entity.User;
 import com.example.distributedtexteditor.repository.DocumentRepository;
