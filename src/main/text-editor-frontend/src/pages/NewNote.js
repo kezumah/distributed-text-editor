e793303ce50afb935a8df9795899ff7fe45a5acd
kezumah <kennedy.ezumah@gmail.com>
2022-12-12 20:55:47 -0800

    publishing changes

--- src/main/text-editor-frontend/src/pages/NewNote.js
+++ src/main/text-editor-frontend/src/pages/NewNote.js
@@ -2,16 +2,42 @@ import { useState } from "react";
 import Note from "../components/Note";
 
 export default function NewNote() {
-  const [code, setCode] = useState("");
+  //const [code, setCode] = useState("");
   const [note, setNote] = useState("");
+
+
+  const socket = new WebSocket('ws://localhost:1300', 'echo-protocol');
+
+  socket.onopen = function() {
+    socket.send(JSON.stringify("hello"));
+    console.log("Socket is opened!");
+  }
+
+  socket.onmessage = function (e) {
+    let message = e.data;
+    console.log(message);
+  }
+
+ // function sendMessage(data) {
+  //  socket.send(data);
+  //  console.log("success")
+  //}
+
+  function handleChange(e) {
+    console.log(e.key);
+   // console.log("Sending to socket server");
+   // sendMessage(e.key);
+  }
+
 
   return (
     <div className="create">
       <lable>Hash:</lable>
-      <input type="text" required value={code} />
+      <input type="text" required value={"abcd"} />
       <textarea
         required
         value={note}
+        onKeyDown={(e) => handleChange(e)}
         onChange={(e) => setNote(e.target.value)}
       ></textarea>
     </div>
